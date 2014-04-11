1516aec49feb243de6aa8dcecaf20f362a76bfe7
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-11 12:28:28 -0400

    all tests in place

--- test.js
+++ test.js
@@ -11,6 +11,7 @@ var multiLine = require('./geojson/MultiLineString')
 var multiPoly = require('./geojson/MultiPolygon')
 
 test('extent', function(t){
+  // FeatureCollection
   var fcExtent = extent(fc)
 
   t.ok(fcExtent)
@@ -19,14 +20,50 @@ test('extent', function(t){
   t.equal(fcExtent[2], 130)
   t.equal(fcExtent[3], 4)
 
-
+  // Point
   var ptExtent = extent(pt)
+
+  t.ok(ptExtent)
+  t.equal(fcExtent[0], 102)
+  t.equal(ptExtent[1], 0.5)
+  t.equal(ptExtent[2], 102)
+  t.equal(ptExtent[3], 0.5)
+
+  // Line
   var lineExtent = extent(line)
+
+  t.ok(lineExtent)
+  t.equal(lineExtent[0], 102)
+  t.equal(lineExtent[1], -10)
+  t.equal(lineExtent[2], 130)
+  t.equal(lineExtent[3], 4)
+
+  // Polygon
   var polyExtent = extent(poly)
+
+  t.ok(polyExtent)
+  t.equal(polyExtent[0], 100)
+  t.equal(polyExtent[1], 0)
+  t.equal(polyExtent[2], 101)
+  t.equal(polyExtent[3], 1)
+
+  // MultiLineString
   var multiLineExtent = extent(multiLine)
+
+  t.ok(multiLineExtent)
+  t.equal(multiLineExtent[0], 100)
+  t.equal(multiLineExtent[1], 0)
+  t.equal(multiLineExtent[2], 103)
+  t.equal(multiLineExtent[3], 3)
+
+  // MultiPolygon
   var multiPolyExtent = extent(multiPoly)
 
-
+  t.ok(multiPolyExtent)
+  t.equal(multiPolyExtent[0], 100)
+  t.equal(multiPolyExtent[1], 0)
+  t.equal(multiPolyExtent[2], 103)
+  t.equal(multiPolyExtent[3], 3)
 
   t.end()
 })
