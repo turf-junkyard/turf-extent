89570e5294811762e6e0b3a99fc06581a14df3c2
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-11 12:06:09 -0400

    experimenting with js geojson exports for browser test support

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ var test = require('tape')
 var fs = require('fs')
 var extent = require('./')
 
+// test data
+var fc = require('./geojson/FeatureCollection')
+var pt  = require('./geojson/Point')
+
+
 test('extent', function(t){
   var fc = require('./geojson/FeatureCollection')
   console.log(fc.type)
