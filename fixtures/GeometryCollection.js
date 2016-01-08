b0db5b085dd0db67e1a773fb6e40c95650472515
Tim Channell <tim.channell@gmail.com>
2016-01-08 17:26:08 -0500

    bump to 1.0.5, eslint, tests

---
+++ fixtures/GeometryCollection.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "type": "GeometryCollection",
+  "geometries": [
+    {
+      "type": "Point",
+      "coordinates": [102.0, 0.5]
+    },
+    {
+      "type": "LineString",
+      "coordinates": [
+        [102.0, -10.0], [103.0, 1.0], [104.0, 0.0], [130.0, 4.0]
+      ]
+    },
+    {
+      "type": "Polygon",
+      "coordinates": [
+        [
+          [20.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0],
+          [100.0, 0.0]
+        ]
+      ]
+    }
+  ]
+}
